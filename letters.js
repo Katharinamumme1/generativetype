1afb6a2adaa2682733ae6ff6cdb4c51b394f529f
Katharinamumme1 <mail@katharinamumme.de>
2025-01-18 13:31:38 +0100

    .

--- letters.js
+++ letters.js
@@ -36,7 +36,7 @@ export function drawLetter(svg, letter, width, baseline, capHeight, ascenderHeig
 
     // Calculate the dynamic height based on the letter's vertical extents
     const dynamicHeight = Math.max(ascenderHeight + descenderHeight, xHeight);
-    svg.setAttribute('viewBox', `-${FIXED_WIDTH / -50} -${descenderHeight} ${FIXED_WIDTH} ${dynamicHeight}`);
+    svg.setAttribute('viewBox', `-${FIXED_WIDTH /100} -${descenderHeight} ${FIXED_WIDTH} ${dynamicHeight}`);
     
     // Center the content horizontally
     const group = document.createElementNS("http://www.w3.org/2000/svg", "g");
